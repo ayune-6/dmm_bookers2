361def15f6d89b10655bcfb8d3b2d1e0df0e3a85
ayune-6 <ayune19990823@gmail.com>
2021-03-06 12:02:28 +0000

    [20210306]FinishedSTEP1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,16 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  # before_action :configure_sign_in_permitted_parameters, if: :devise_controller?
 
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email])
+    # devise_parameter_sanitizer.permit(:sign_in, keys: [:username])
   end
+
+  # def configure_sign_in_permitted_parameters
+  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:username])
+  # end
 
 end
