f6d38addc53609a7f12574a00d69c82f52f12f68
ayune-6 <ayune19990823@gmail.com>
2021-03-07 08:33:36 +0000

    [20210307]ImplementingUserProfilePage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @books = @users.books.page(params[:page]).reverse_order
+    @books = @user.books.page(params[:page]).reverse_order
+    
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/show'
   devise_for :users
   root :to => 'homes#top'
   resources :books, only: [:new, :create, :index, :show, :destroy]
