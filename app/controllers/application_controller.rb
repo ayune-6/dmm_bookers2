51ff4e3e81c73960513a7f0ee732cfd6edb9a525
ayune-6 <ayune19990823@gmail.com>
2021-03-07 12:15:20 +0000

    [20210307]DoneWithProfileFunctions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,8 @@ class ApplicationController < ActionController::Base
     user_path(resource)
   end
 
+
+
   protected
 
   def configure_permitted_parameters
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   root :to => 'homes#top'
   resources :books, only: [:new, :create, :index, :show, :destroy]
 
-  resources :users, only: [:show]
+  resources :users, only: [:show, :index, :edit, :update]
 end
