bf302f239d12ae53ee7831bcdbf1ca10dbd6a553
ayune-6 <ayune19990823@gmail.com>
2021-03-10 12:46:29 +0000

    [20210310]ImplementingBootstrap

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
     @newbook = Book.new
-    @info_user = User.find(params[:id])
+    # @info_user = User.find(params[:id])
     @user = User.find(params[:id])
     @books = @user.books.page(params[:page]).reverse_order
   end
