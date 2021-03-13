64edc88fbd60ebea9c67897a938a59578c644502
ayune-6 <ayune19990823@gmail.com>
2021-03-13 05:10:28 +0000

    [20210313]FixedBootstrapinLayouts

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :books, dependent: :destroy
+  has_many :favorites, dependent: :destroy
   validates :name, uniqueness: true, length: { in: 2..20 }
   validates :introduction, length: { maximum: 50 }
   attachment :profile_image
